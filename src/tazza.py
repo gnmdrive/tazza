bf9cb3ee0bfac33981972baf772495300ecccc7c
Gianmarco Guida <giammyguida05@mail.com>
2023-05-05 20:03:11 +0200

    first implementation of function that detect token type

--- src/tazza.py
+++ src/tazza.py
@@ -37,6 +37,16 @@ class TokenType(Enum):
     STRING_LITERAL = auto()
     CONST_NUMBER = auto()
     IDENTIFIER = auto()
+    IDENTIFIER_CALL = auto()
+    KEYWORD = auto()
+    DATA_TYPE = auto()
+    ROUND_PAREN = auto()
+    SQUARE_PAREN = auto()
+    CURLY_PAREN = auto()
+    OPERATOR = auto()
+    EQUALS = auto()
+    TYPE_DEFINER = auto()
+    INSTRUCTION_SEPARATOR = auto()
 
 @dataclass(slots=True, kw_only=True)
 class Token:
@@ -50,6 +60,57 @@ def find_end(predicate, begin: int, line: str) -> int:
     while predicate(line, end):
         end += 1
     return end + 1 if line[begin] == '"' else end # return "****"
+
+def search_type_definer(line: str, end: int) -> int:
+    col = end
+    line_length = len(line)
+    while col < line_length:
+        if not line[col].isspace():
+            break
+        if line[col] == ':':
+            return col
+        col += 1
+    return -1
+
+def get_token_from_alpha(line: str, begin: int, end: int) -> TokenType:
+    # get keyword or identifier
+    KEYWORDS = ['for', 'if', 'else', 'elseif', 'while', 'break', 'continue', 'struct']
+    DATA_TYPES = ['int', 'float', 'string', 'array']
+    alpha = line[begin:end]
+    if alpha in KEYWORDS:
+        return TokenType.KEYWORD
+    elif alpha in DATA_TYPES:
+        return TokenType.DATA_TYPE
+    elif search_type_definer(line, end) != -1:
+        return TokenType.IDENTIFIER
+    return TokenType.IDENTIFIER_CALL
+    # return TokenType.UNKNOWN
+
+def get_token_from_symbol(line: str, begin: int, end: int) -> TokenType:
+    # get meaning of symbol
+    PAREN = '()[]{}'
+    SYMBOLS = '=;:'
+    OPERATORS = ['+', '-', '*', '/']
+    symbol = line[begin:end]
+    if symbol in PAREN:
+        match symbol:
+            case '(' | ')':
+                return TokenType.ROUND_PAREN
+            case '[' | ']':
+                return TokenType.SQUARE_PAREN
+            case '{' | '}':
+                return TokenType.CURLY_PAREN
+    elif symbol in OPERATORS:
+        return TokenType.OPERATOR
+    elif symbol in SYMBOLS:
+        match symbol:
+            case '=':
+                return TokenType.EQUALS
+            case ':':
+                return TokenType.TYPE_DEFINER
+            case ';':
+                return TokenType.INSTRUCTION_SEPARATOR
+    return TokenType.UNKNOWN
 
 def main():
     argv = sys.argv
@@ -85,10 +146,10 @@ def main():
     print(detail)
 
     with open(detail.file_path, 'r') as f:
-        SYMBOLS = '=;:(){}[]+-*/'
-        source: list[str] = f.read().splitlines(keepends=True)
+        source: str = f.read()
+        splittedlines: list[str] = source.splitlines(keepends=True)
         list_of_tokens: list[Token] = []
-        for row, line in enumerate(source):
+        for row, line in enumerate(splittedlines):
             # for each line in source
             col, line_length = 0, len(line)
             while col < line_length:
@@ -97,7 +158,7 @@ def main():
                 token_begin, token_end = col, None
                 if line[col].isalpha():
                     token_end = find_end(lambda l, c: l[c].isalpha(), token_begin, line) # keyword or identifier 
-                    # token_type = get_token_from_alpha()
+                    token_type = get_token_from_alpha(line, token_begin, token_end)
                 elif line[col] == '"':
                     token_end = find_end(lambda l, c: l[c] != '"' or l[c-1] == '\\', token_begin, line) # string literal 
                     token_type = TokenType.STRING_LITERAL
@@ -112,11 +173,8 @@ def main():
                     if line[col] == '/' and line[col + 1] in ['n', '/']:
                         break # no more token on this line
                     token_end = token_begin + 1
-                    # if line[col] not in SYMBOLS:
-                    #     compilation_error(f'`{line[col]}` not recognized', row, col, detail.file_path)
-                    # token_type = get_token_from_symbol(line, token_begin, token_end)
+                    token_type = get_token_from_symbol(line, token_begin, token_end)
 
-                
                 col = token_end
                 list_of_tokens.append(Token(ttype=token_type, txt=line[token_begin:token_end], row=row, col=token_begin))
 
